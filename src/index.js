dfeb64bc3650171d361c732c1a4c71811c7bf9f0
healim01 <22000770@handong.edu>
2023-07-07 02:53:08 +0900

    feat: basic route

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from "react-query";
+// import { ThemeProvider } from "styled-components";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
+import App from "./App";
+// import { theme } from "./theme";
+
+const queryClient = new QueryClient();
+
+ReactDOM.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      {/* <ThemeProvider theme={theme}> */}
+        <App />
+      {/* </ThemeProvider> */}
+    </QueryClientProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );

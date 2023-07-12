b2e86a8bb271799cc8bd429b7616bb6ca7ab772a
healim01 <22000770@handong.edu>
2023-07-13 05:44:24 +0900

    feat: change router

--- src/Router.js
+++ src/Router.js
@@ -2,21 +2,23 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Funding from './routes/Funding';
 import MakeFund from './routes/MakeFund';
 import Room from './routes/Room';
+import Home from './routes/Home';
 
 function Router() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/:RoomId/make">
-          <MakeFund />
+        <Route exact path="/">
+          <Home />
         </Route>
-        <Route path="/:RoomId">
+        <Route exact path="/room/:roomId">
           <Room />
         </Route>
-        <Route path="/">
-          {' '}
-          {/* /:product_id ==> room 내부로 바꿔야함 */}
+        <Route exact path="/room/:roomId/funding/:fundingId">
           <Funding />
+        </Route>
+        <Route exact path="/room/:roomId/make">
+          <MakeFund />
         </Route>
       </Switch>
     </BrowserRouter>

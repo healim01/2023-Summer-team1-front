9d0dbbc08240b41ee3691bbda9b0a0c40234a08d
healim01 <22000770@handong.edu>
2023-07-13 05:44:54 +0900

    feat: funding page api

--- src/routes/Funding.js
+++ src/routes/Funding.js
@@ -3,46 +3,24 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FundingDetail from './FundingDetail';
 import PopUp from '../components/PopUp';
 import Header from '../components/Header';
-import { useEffect } from 'react';
-import { getTwoFunding } from '../apis/funding';
-
-const fund = {
-  funding_id: 1,
-  host_id: 'danaka',
-  seller_id: 'healim01',
-  category_id: '굿즈',
-  goal_amount: 1000000,
-  is_active: 3,
-  created_date: '2023-06-23',
-  title: '보송보송 여우 보송이 키링 인형',
-  image:
-    'https://tumblbug-pci.imgix.net/da55d49c72146cd5042c3c71153d4c0712ef9277/29b42ad5398ae28327d1e996a009f0e58464a1b4/f8fb2e4966d583fc2bc0c490ae456479e1d0083a/75b84011-2d99-4db5-9c4e-afab213e4e98.jpeg?ixlib=rb-1.1.0&w=1240&h=930&auto=format%2Ccompress&lossless=true&fit=crop&s=b571cc96917ee605da1e11b26bacc131',
-  price: 10000,
-  start_date: Date.now(),
-  end_date: '2023.07.30.',
-  biling_date: '2023.08.01.',
-  likes: 40,
-};
-
-const fund_amounts = {
-  funding_id: 1,
-  current_amount: 3000000,
-  backers_count: 300,
-};
+import { getFunding } from '../apis/fundingApi';
+import Footer from '../components/Footer';
+import { useQuery } from 'react-query';
+import { useParams } from 'react-router-dom';
 
 const Fund = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   text-align: center;
-  min-width: 980px;
+  min-width: 1000px;
   padding: 50px 0px;
 `;
 
 const Category = styled.div`
-  font-size: 13px;
+  font-size: 18px;
   width: 100px;
-  height: 30px;
+  height: 40px;
   border: 1px solid lightgray;
   display: flex;
   justify-content: center;
@@ -51,7 +29,7 @@ const Category = styled.div`
 `;
 
 const ProTitle = styled.div`
-  font-size: 45px;
+  font-size: 50px;
   font-weight: bold;
   margin-bottom: 50px;
 `;
@@ -63,11 +41,11 @@ const Grids = styled.div`
   gap: 50px;
 `;
 
-const Grid = styled.div``;
+const Container = styled.div``;
 
 const ProImg = styled.img`
-  width: 600px;
-  height: 450px;
+  width: 650px;
+  height: 500px;
 `;
 
 const CurrContent = styled.div`
@@ -77,7 +55,7 @@ const CurrContent = styled.div`
 `;
 
 const CurrContentTitle = styled.div`
-  font-size: 14px;
+  font-size: 16px;
   margin-bottom: 5px;
 `;
 
@@ -88,18 +66,19 @@ const ItemDetail = styled.div`
 
 const CurrContentItem = styled.div`
   font-weight: 400;
-  font-size: 40px;
+  font-size: 45px;
   margin-bottom: 10px;
 `;
 
 const Detail = styled.div`
-  font-size: 14px;
+  font-size: 16px;
   margin-bottom: 20px;
   margin-left: 5px;
   margin-right: 10px;
 `;
 
 const Percentage = styled(Detail)`
+  font-size: 21px;
   font-weight: 400;
 `;
 
@@ -116,7 +95,7 @@ const GoalContent = styled.div`
 `;
 
 const GoalContentItem = styled.span`
-  font-size: 12px;
+  font-size: 16px;
 `;
 
 const GoalContentTitle = styled(GoalContentItem)`
@@ -141,10 +120,9 @@ const Likes = styled.button`
   font-size: 13px;
   background-color: white;
   &:active {
+    color: red;
   }
 `;
-
-const ratio = (fund_amounts.current_amount / fund.goal_amount) * 100;
 
 const formatDate = timestamp => {
   const date = new Date(timestamp);
@@ -155,44 +133,50 @@ const formatDate = timestamp => {
   return `${year}/${month}/${day}`;
 };
 
-function Funding() {
-  useEffect(() => {
-    const fetchApi = async () => {
-      const data = await getTwoFunding(1, 1);
-      console.log(data);
-    };
-    fetchApi();
-  }, []);
+const converter = num => {
+  if (typeof num === 'undefined') {
+    return '';
+  }
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
 
-  fund_amounts.current_amount = fund_amounts.current_amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-  fund.goal_amount = fund.goal_amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-  fund.price = fund.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+function Funding() {
+  const { roomId, fundingId } = useParams();
+
+  const { isLoading, data: fund } = useQuery(['fund', roomId], () => getFunding(fundingId), {
+    onSuccess: data => {
+      console.log('Funding data:', data);
+    },
+  });
+
+  console.log(fund);
+
   return (
     <>
       <Header />
       <Fund>
-        <Category> {fund.category_id} </Category>
-        <ProTitle> {fund.title} </ProTitle>
+        <Category> {fund?.category_name} </Category>
+        <ProTitle> {fund?.funding_title} </ProTitle>
         <Grids>
-          <Grid>
-            <ProImg src={fund.image} />
-          </Grid>
-          <Grid>
+          <Container>
+            <ProImg src={fund?.funding_image} />
+          </Container>
+          <Container>
             <CurrContent>
               <CurrContentTitle> 모인 금액 </CurrContentTitle>
               <ItemDetail>
-                <CurrContentItem> {fund_amounts.current_amount} </CurrContentItem>
+                <CurrContentItem> {converter(fund?.current_amount)} </CurrContentItem>
                 <Detail>원</Detail>
-                <Percentage> {ratio}%</Percentage>
+                <Percentage> {fund?.progress}%</Percentage>
               </ItemDetail>
               <CurrContentTitle> 후원자 </CurrContentTitle>
               <ItemDetail>
-                <CurrContentItem> {fund_amounts.backers_count} </CurrContentItem>
+                <CurrContentItem> {converter(fund?.backers_count)} </CurrContentItem>
                 <Detail>명</Detail>
               </ItemDetail>
               <CurrContentTitle> 후원 금액 </CurrContentTitle>
               <ItemDetail>
-                <CurrContentItem> {fund.price} </CurrContentItem>
+                <CurrContentItem> {converter(fund?.price)} </CurrContentItem>
                 <Detail> 원 + </Detail>
               </ItemDetail>
               <Detail> 배송비 포함(제주도/도서산간:3,000원 추가 후원) </Detail>
@@ -200,32 +184,30 @@ function Funding() {
             <Hr />
             <GoalContent>
               <GoalContentTitle> 목표 금액 </GoalContentTitle>
-              <GoalContentItem> {fund.goal_amount}원 </GoalContentItem>
+              <GoalContentItem> {converter(fund?.goal_amount)}원 </GoalContentItem>
             </GoalContent>
             <GoalContent>
               <GoalContentTitle> 펀딩 기간 </GoalContentTitle>
               <GoalContentItem>
-                {' '}
-                {formatDate(fund.start_date)} ~ {formatDate(fund.end_date)}{' '}
+                {formatDate(fund?.start_date)} ~ {formatDate(fund?.end_date)}
               </GoalContentItem>
-              {/* TODO :: 며칠 남았는지 작업 안함 */}
             </GoalContent>
             <GoalContent>
               <GoalContentTitle> 결제 </GoalContentTitle>
-              <GoalContentItem> 목표금액 달성시 {formatDate(fund.biling_date)}에 결제 진행 </GoalContentItem>
+              <GoalContentItem> 목표금액 달성시 {formatDate(fund?.budget_date)}에 결제 진행 </GoalContentItem>
             </GoalContent>
             <GridsButtons>
               <Likes>
                 <FavoriteBorderIcon fontSize="small" />
-                {fund.likes}
+                {fund?.like_num}
               </Likes>
               <PopUp />
             </GridsButtons>
-          </Grid>
+          </Container>
         </Grids>
-        <FundingDetail />
+        <FundingDetail fundDetail={fund} />
       </Fund>
-      ;
+      <Footer />
     </>
   );
 }

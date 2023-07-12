b8692dd021bd7309bdc3df8921771330fb0c6fb9
healim01 <22000770@handong.edu>
2023-07-13 05:43:08 +0900

    feat: apis

---
+++ src/apis/fundingApi.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+// GET
+export const getFunding = async funding_id => {
+  const response = await axios.get(`http://localhost:8080/funding/${funding_id}`);
+  return response.data;
+};
+
+export const getRoom = async room_id => {
+  const response = await axios.get(`http://localhost:8080/room/${room_id}`);
+  return response.data;
+};
+
+export const getTop4Room = async () => {
+  const response = await axios.get(`http://localhost:8080/main/room-top4`);
+  return response.data;
+};
+
+export const getAllFunding = async () => {
+  const response = await axios.get(`http://localhost:8080/main/funding-allList`);
+  return response.data;
+};
+
+// POST
+export const postApi = async data => {
+  const response = await axios.post(`http://localhost:8080/room`, data);
+  return response;
+};
+
+export const postApis = async pet => {
+  const response = await axios.fetch(`http://localhost:8080/room`, pet);
+  return response;
+};

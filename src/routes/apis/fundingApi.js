88f41cbd3443d9e5c2ba8d73e7c091a3c491a2cd
healim01 <22000770@handong.edu>
2023-07-13 05:44:40 +0900

    rm: rm

--- src/routes/apis/fundingApi.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const getTwoFunding = async (room_id, funding_id) => {
-  const response = await axios.get(`https://localhost:8080/room/${room_id}/funding/${funding_id}`);
-  return response.data;
-};

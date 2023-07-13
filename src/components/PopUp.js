b031369532be69cdcd768f54c277815501242daa
healim01 <22000770@handong.edu>
2023-07-14 08:12:03 +0900

    Update PopUp.js

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -2,51 +2,88 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-// import DialogContentText from '@mui/material/DialogContentText';
+// import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import styled from 'styled-components';
+import axios from 'axios';
+import LogoImg from '../imgs/logo.png';
 
 const Buy = styled.button`
   font-size: 16px;
-  width: 250px;
+  width: 350px;
   height: 50px;
-  background-color: red;
+  background-color: #6314e7;
   color: white;
   font-weight: bold;
   border: 0;
   border-radius: 5px;
 `;
 
-export default function PopUp() {
+const PopUpContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 200px;
+  width: 350px;
+`;
+
+const PopLogo = styled.img`
+  width: 50px;
+`;
+
+const PopText = styled.div`
+  text-align: center;
+  margin-top: 20px;
+`;
+
+export default function PopUp(props) {
   const [open, setOpen] = React.useState(false);
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+  // const handleClickOpen = () => {
+  //   setOpen(true);
+  // };
   const handleClose = () => {
     setOpen(false);
   };
 
+  // const button = document.querySelector('#your-button-id');
+  const postBacker = async () => {
+    try {
+      const response = axios.post(`http://localhost:8080/backer/15/${props.funding_id}`, {
+        fund_num: 1,
+      });
+      setOpen(true);
+
+      console.log(response); // 응답 확인 (선택사항)
+    } catch (error) {
+      console.error(error);
+      // 에러 처리
+    }
+  };
+
   return (
     <div>
-      <Buy onClick={handleClickOpen}> 이 프로젝트 후원하기</Buy>
+      <Buy onClick={postBacker}> 이 프로젝트 후원하기</Buy>
       <Dialog
         open={open}
         onClose={handleClose}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
+        maxWidth="md"
       >
-        <DialogTitle id="alert-dialog-title">{'프로젝트에 후원해주셔서 감사합니다'}</DialogTitle>
-        <DialogContent>
-          {/* <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending anonymous
-            location data to Google, even when no apps are running.
-          </DialogContentText> */}
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>나의 펀딩 내역 확인하기 </Button>
-          <Button onClick={handleClose} autoFocus>
+        <DialogTitle id="alert-dialog-title">
+          <PopUpContainer>
+            <PopLogo src={LogoImg} />
+            <PopText>
+              예약 구매가 완료되었습니다.
+              <br />
+              프로젝트에 후원해주셔서 감사합니다.
+            </PopText>
+          </PopUpContainer>
+        </DialogTitle>
+        <DialogActions sx={{ display: 'flex', justifyItems: 'center' }}>
+          <Button onClick={handleClose} autoFocus sx={{ backgroundColor: 'red' }}>
             펀딩 계속하기
           </Button>
         </DialogActions>
